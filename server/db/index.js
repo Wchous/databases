8f995ec23d7f6d90b5e3e14ffd6f079b1efc98f3
wchous <wchoustoun@gmail.com>
2018-07-06 17:20:20 -0500

    set tables, start server integration

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,23 @@
 var mysql = require('mysql');
 
 // Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
+// You will need to connect with the user "student", password "student",
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host      : 'localhost',
+  user      : 'student',
+  password  : 'student',
+  database  : 'chat' 
+})
 
+connection.connect();
+
+connection.query('SELECT * FROM messages', function(err, rows, cols){
+  if (err){
+    console.log('there has been an error');
+  }
+  console.log('The message in the first column is' + rows[0].message)
+})
+
+connection.end();
--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,40 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE chat_rooms(
+  room_id INTEGER PRIMARY KEY,
+  room_name TEXT,
+  room_description TEXT 
+);
+CREATE TABLE users(
+  user_id INTEGER PRIMARY KEY,
+  user_name TEXT,
+  user_karma INTEGER
 );
 
-/* Create other tables and define schemas for them here! */
+INSERT INTO chat_rooms(room_id, room_name, room_description)
+VALUES (1, 'Tonys Place', 'where tony hangs out');
+
+INSERT INTO users(user_id, user_name, user_karma)
+VALUES (1, 'tony', 8000);
+CREATE TABLE messages (
+  message_id INTEGER PRIMARY KEY,
+  message TEXT,
+  created_at DATE,
+  user_id INTEGER,
+  room_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  FOREIGN KEY(room_id) REFERENCES chat_rooms(room_id) 
+  -- FOREIGN KEY REFERENCES chat_rooms(room_id)
+);
+
+INSERT INTO messages (message_id, message, created_at, user_id, room_id)
+VALUES (1, 'foo', '06/07/18', 1, 1);
+
+SELECT * FROM messages;
 
 
 
